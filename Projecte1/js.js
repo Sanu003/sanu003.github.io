bf5e2429874fc8d115b6f10c6dc0e036bdb04e04
Sanu003 <117604020+Sanu003@users.noreply.github.com>
2023-06-26 04:32:18 +0200

    Add files via upload

---
+++ Projecte1/js.js
@@ -0,0 +1,58 @@
+let enllac = location.href.substring(22,23)
+let imatgesenllac="./imatges/"
+
+console.log(location.href)
+
+console.log(enllac)
+
+let div = document.getElementById("div1")
+
+//Crear Comics//////////////////////////////////////////
+if (enllac==""){
+    for (let i =1; i<14;i++){
+        var k = document.createElement("img")
+        k.src = 'imatges/'+i+'/1.png'
+        k.width = 600
+        k.height = 800
+        k.style.marginLeft="20px"
+        k.style.marginTop="10px"
+        div.appendChild(k)
+        k.addEventListener("click", algo)
+            
+        function algo(){
+            open("imatges/"+i+"/index1.html")
+        }  
+    }
+    CrearImatges()
+}else
+CrearImatges()
+
+
+//Crear Imatges/////////////////////////////////////////
+function CrearImatges(){
+    for (let i =1; i<274; i++){
+        var j = document.createElement("img")
+        if (enllac==""){
+        var url = 'imatges/'+i+'.png'
+        }else{
+            var url = 'imatges/'+num+'/'+i+'.png'
+        }
+        j.id=i
+        try{
+            j.src = url
+        }catch{}     
+        if(j.height!=0){
+            j.width = 600
+            j.height = 800
+            j.style.marginLeft="20px"
+            j.style.marginTop="10px"
+            div.appendChild(j)
+            j.addEventListener("click", algo)
+        }
+        function algo(){
+            var pantalla= window.open("imatges/"+i+".png", '_blank')
+            window.focus();
+            pantalla.blur()
+        }    
+    }
+}
---
+++ Projecte1/js2.js
@@ -0,0 +1,27 @@
+let div = document.getElementById("div1")
+let num = location.href.substring(30,31)
+console.log(num)
+CrearImatges()
+
+//Crear Imatges/////////////////////////////////////////
+function CrearImatges(){
+    for (let i =1; i<274; i++){
+        var j = document.createElement("img")
+        var url = i+'.png'
+        j.id=i
+        j.src = url
+        if(j.height!=0){
+            j.width = 920
+            j.height = 800
+            j.style.marginLeft="20px"
+            j.style.marginTop="10px"
+            div.appendChild(j)
+            j.addEventListener("click", algo)
+        }
+        function algo(){
+            var pantalla= window.open(i+'.png', '_blank')
+            window.focus();
+            pantalla.blur()
+        }    
+    }
+}
